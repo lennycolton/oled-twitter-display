897a1e1845a187d4e755c2f0b06792d7f8a47a9a
lennycolton <40361891+lennycolton@users.noreply.github.com>
2018-06-19 10:24:00 +0100

    Bug fix + clock display

--- oledAPIDisplay.py
+++ oledAPIDisplay.py
@@ -3,7 +3,7 @@ import json
 import base64
 import urllib3
 http = urllib3.PoolManager()
-from OmegaExpansion import oledExp
+f#rom OmegaExpansion import oledExp
 
 baseUrl     = "https://jsonplaceholder.typicode.com/"
 bearerToken = ""
@@ -18,20 +18,20 @@ def getAPIData():
     )
     return data
 
-def oledWriteData(text):
-    if oledExp.driverInit() != 0:
-        print 'ERROR: Could not initialize the OLED Expansion'
-        return False
-
-    # clear the display
-    oledExp.clear()
-
-
-    # set the cursor to the next line
-    oledExp.setCursor(0,0)
-
-    # write out the tweet
-    oledExp.write(text)
+##def oledWriteData(text):
+##    if oledExp.driverInit() != 0:
+##        print 'ERROR: Could not initialize the OLED Expansion'
+##        return False
+##
+##    # clear the display
+##    oledExp.clear()
+##
+##
+##    # set the cursor to the next line
+##    oledExp.setCursor(0,0)
+##
+##    # write out the tweet
+##    oledExp.write(text)
 
 
 
@@ -41,14 +41,14 @@ def mainProgram():
     # use api to get data
     data= getAPIData()
     if not data:
-        print "ERROR: Could not retreive data!"
+        print("ERROR: Could not retreive data!")
         exit()
 
-    print 'Got data! ', data
+    print('Got data! ', data)
     # display the tweet on the OLED
-    oledWriteData(data['body'])
+##    oledWriteData(data['body'])
 
-    print 'Done!'
+    print('Done!')
 
 
 if __name__ == "__main__":
