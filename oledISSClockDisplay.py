63bc5190dfebbc5176621213a7eef93e2eba2016
lennycolton <40361891+lennycolton@users.noreply.github.com>
2018-06-19 12:34:59 +0100

    Added human-readable location

--- oledISSClockDisplay.py
+++ oledISSClockDisplay.py
@@ -14,7 +14,10 @@ currentdate = ""
 currentlat = ""
 currentlon = ""
 
+place = False
+
 while True:
+    place = not place
     prevlat = currentlat
     prevlon = currentlon
     prevtime = currenttime
@@ -28,30 +31,56 @@ while True:
     obj = json.loads(response.read())
 
     if currenttime != prevtime:
-        oledExp.setCursor(0,0)
-        oledExp.write(currenttime)
-        
-        req = urllib2.Request("http://nominatim.openstreetmap.org/reverse?format=json&lat="obj['iss_position']['latitude']"&lon="obj['iss_position']['longitude']"&zoom=18&addressdetails=1")
-        response = urllib2.urlopen(req)
-
-        mapobj = json.loads(response.read())
+        if not place:
+            oledExp.setCursor(0,0)
+            oledExp.write(currenttime)
 
     if currentdate != prevdate:
-        oledExp.setCursor(2,0)
-        oledExp.write(currentdate)
+        if not place:
+            oledExp.setCursor(2,0)
+            oledExp.write(currentdate)
 
     currentlat = obj['iss_position']['latitude']
     if currentlat != prevlat:
-        oledExp.setCursor(4,0)
-        if float(currentlat) < 0:
-            oledExp.write(str(currentlat)[1:] + ' S    ')
-        else:
-            oledExp.write(str(currentlat) + ' N    ')
+        if not place:
+            oledExp.setCursor(4,0)
+            if float(currentlat) < 0:
+                oledExp.write(str(currentlat)[1:] + ' S    ')
+            else:
+                oledExp.write(str(currentlat) + ' N    ')
 
     currentlon = obj['iss_position']['longitude']
     if currentlon != prevlon:
-        oledExp.setCursor(6,0)
-        if float(currentlon) < 0:
-            oledExp.write(str(currentlon)[1:] + ' W    ')
-        else:
-            oledExp.write(str(currentlon) + ' E    ')
+        if not place:
+            oledExp.setCursor(6,0)
+            if float(currentlon) < 0:
+                oledExp.write(str(currentlon)[1:] + ' W    ')
+            else:
+                oledExp.write(str(currentlon) + ' E    ')
+
+    if place:
+        if currenttime != prevtime:
+            mapreq = urllib2.Request("http://nominatim.openstreetmap.org/reverse?format=json&lat="obj['iss_position']['latitude']"&lon="obj['iss_position']['longitude']"&zoom=18&addressdetails=1")
+            mapresponse = urllib2.urlopen(mapreq)
+
+            mapobj = json.loads(mapresponse.read())
+            
+            town = mapobj['hamlet']
+            county = mapobj['county']
+            state = mapobj['state']
+            country = mapobj['country']
+
+            oledExp.clear()
+
+            oledExp.setCursor(0,0)
+            oledExp.write(hamlet)
+
+            oledExp.setCursor(2,0)
+            oledExp.write(county)
+
+            oledExp.setCursor(4,0)
+            oledExp.write(state)
+
+            oledExp.setCursor(6,0)
+            oledExp.write(country)
+            
